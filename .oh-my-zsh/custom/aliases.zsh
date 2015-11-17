5f527415605f5f9407e6f75c56d64509899a6f25
Ivan Stiazkin <i_stiazkin@wargaming.net>
2015-11-17 12:32:54 +0300

    oh-my-zsh custom aliases

---
+++ .oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,14 @@
+alias gcm='git commit -m'
+alias gc!='git commit --amend'
+
+alias ga= 'git add'
+alias gau='git add -u :/'
+
+alias igrep='grep -i'
+
+alias rundjango='./manage.py runserver 127.0.0.1:8008'
+alias ipy='ipython'
+
+alias sshrutrunk='ssh wgnwn-wgt1@ru.wgnwn.wgnt.iv'
+
+alias sshnesusvet='ssh nesusvet@dev.nesusvet.com'
